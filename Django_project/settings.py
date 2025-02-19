0a7a2da3fa1803ec359202d643c35dafe1d586be
Prakash11100 <149244289+Prakash11100@users.noreply.github.com>
2025-02-19 18:10:03 +0530

    settings.py

--- Django_project/settings.py
+++ Django_project/settings.py
@@ -82,16 +82,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}'''
+
 
 
 # Password validation
